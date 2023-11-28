dca4a2ea1922c0ed97bfc20ce3338f80082abcdc
Drew Fowler <drew.fowler@aed.pro>
2023-11-28 13:02:50 -0700

    added matrix

--- pibatoid/keymaps/default/keymap.c
+++ pibatoid/keymaps/default/keymap.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_0(
                                                                   //       ┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┐       ┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┐
                                                                   //       |         |         |         |         |         |         |       |         |         |         |         |         |         |
                                                                   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
